9df49dd027d2dc64714e5cd64c28b7d6f458f264
Poojitha Manam <65291819+Poojitha-Chandra@users.noreply.github.com>
2021-03-27 14:22:33 +0530

    Add files via upload

--- TruyumConsole/TruyumConsole/Com.Cognizant.Truyum.Utility/DateUtility.cs
+++ TruyumConsole/TruyumConsole/Com.Cognizant.Truyum.Utility/DateUtility.cs
@@ -1,1 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Com.Cognizant.Truyum.Utility
+{
+    public class DateUtility
+    {
+        //converts the input date entered in string format to Datetime
+        public static DateTime ConvertToDate(string inputDate)
+        {
+            return DateTime.Parse(inputDate);
+        }
+    }
+}
