43b601c982655e66ca977f71ffbe86feb2c45e52
Poojitha Manam <65291819+Poojitha-Chandra@users.noreply.github.com>
2021-03-27 14:23:22 +0530

    Add files via upload

---
+++ TruyumConsole/TruyumConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Cognizant.Truyum.Dao;
+
+namespace TruyumConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            MenuItemDaoCollectionTest.TestGetMenuItemListAdmin();
+            MenuItemDaoCollectionTest.TestGetMenuItemListCustomer();
+            //  MenuItemDaoCollectionTest.TestModifyMenuItem();
+            // MenuItemDaoCollectionTest.TestGetMenuItem();
+
+            /*    CartDaoCollectionTest.TestAddCartItem();
+                CartDaoCollectionTest.TestGetAllCartItems();
+                CartDaoCollectionTest.TestRemoveCartItem();*/
+
+            // Test add item to cart
+            CartDaoCollectionTest.TestAddCartItem();
+        }
+    }
+}
