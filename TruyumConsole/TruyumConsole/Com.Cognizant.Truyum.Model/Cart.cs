3c917b26b4bc64ac9ade69fc47a3cebf14290e9c
Poojitha Manam <65291819+Poojitha-Chandra@users.noreply.github.com>
2021-03-27 14:20:41 +0530

    Add files via upload

---
+++ TruyumConsole/TruyumConsole/Com.Cognizant.Truyum.Model/Cart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Cognizant.Truyum.Model
+{
+    public class Cart
+    {
+        private List<MenuItem> menuItemList;
+        private double total;
+
+        //properties
+        public List<MenuItem> MenuItemList
+        {
+            get => menuItemList;
+            set { menuItemList = value; }
+        }
+        public double Total
+        {
+            get => total;
+            set { total = value; }
+        }
+
+        public Cart() { }
+
+        //parameterized constructor
+        public Cart(List<MenuItem> menuItemList, double total)
+        {
+            MenuItemList = menuItemList;
+            Total = total;
+        }
+
+        //generate ToString() method
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        //generate Equals() method
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ TruyumConsole/TruyumConsole/Com.Cognizant.Truyum.Model/MenuItem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Cognizant.Truyum.Model
+{
+    public class MenuItem
+    {
+        private long id;
+        private string name;
+        private float price;
+        private bool active;
+        private DateTime dateOfLaunch;
+        private string category;
+        private bool freeDelivery;
+
+        //properties
+        public long Id
+        {
+            get => id;
+            set { id = value; }
+        }
+        public string Name
+        {
+            get => name;
+            set { name = value; }
+        }
+        public float Price
+        {
+            get => price;
+            set { price = value; }
+        }
+        public bool Active
+        {
+            get => active;
+            set { active = value; }
+        }
+        public DateTime DateOfLaunch
+        {
+            get => dateOfLaunch;
+            set { dateOfLaunch = value; }
+        }
+        public string Category
+        {
+            get => category;
+            set { category = value; }
+        }
+        public bool FreeDelivery
+        {
+            get => freeDelivery;
+            set { freeDelivery = value; }
+        }
+        public MenuItem() { }
+
+        // parameterized constructor
+        public MenuItem(long id, string name, float price, bool active, DateTime dateOfLaunch, string category, bool freeDelivery)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Active = active;
+            DateOfLaunch = dateOfLaunch;
+            Category = category;
+            FreeDelivery = freeDelivery;
+        }
+
+        //generated ToString() method
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        //generated Equals() method
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+    }
+}
